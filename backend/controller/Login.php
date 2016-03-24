6da65610264569d5926f388dd9295e5ef84cfbaa
yzb <753494666@qq.com>
2016-03-24 22:22:41 +0800

    register

--- backend/controller/Login.php
+++ backend/controller/Login.php
@@ -1,7 +1,20 @@
 <?php
+namespace controller;
 
-class Login
+/**
+ * 登陆接口
+ * v1.0
+ * @author 叶子彬
+ * @version 2016年3月24号
+ * 
+ */
+
+class Login extends CheckIp
 {
+    public function run()
+    {
+        
+    }
 }
 
 ?>
