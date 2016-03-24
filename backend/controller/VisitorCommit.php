a5c9a68111ff3337213e05c09feddb6b4a1ee4d4
unknown <>
2016-03-24 16:44:10 +0800

    kiky

--- backend/controller/VisitorCommit.php
+++ backend/controller/VisitorCommit.php
@@ -1,20 +1,31 @@
 <?php
-namespace controller;
-
-use model\Answer;
-use vendor\MyRedis;
-use vendor\Output;
 /**
  * Created by PhpStorm.
  * User: kikym
  * Date: 2016/3/24
  * Time: 14:19
  */
+namespace controller;
+
+use model\Answer;
+use model\Util;
+use vendor\MyRedis;
+use vendor\Output;
+
 class VisitorCommit extends Action
 {
     public function run(Output $output)
     {
+        $data = I('post.data');
+        if (empty($data)) {
+            exit;
+        }
 
+        $answer = new answer();
+        $answer->value = $data;
+        $answer->save();
+
+        output(0,'','游客提交成功');
 
     }
 }
