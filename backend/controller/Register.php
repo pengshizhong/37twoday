8cc8320e6c5b3ed534dd3e305adf22073b74ee87
yzb <753494666@qq.com>
2016-03-24 15:34:38 +0800

    ''

--- backend/controller/Register.php
+++ backend/controller/Register.php
@@ -10,24 +10,32 @@ namespace controller;
 use model\User;
 use vendor\MyRedis;
 use vendor\Output;
+use model\Db;
 class Register extends Action
 {
     public function run()
     {
-        
+        //$res = Db::query("SELECT * from user where USER_ID = :user_id", array(":user_id"=>1));
+        $user = new User();
+        $res = $user->select(['USER_ID' => 1]);
+        p($res);
     }
     
     private function getName()
     {
-        $username = I("post.username");
+        $user = new User();
+        $workid = I("post.workid");
         if(self::$me->get($username)){
-            output(1, 0, self::$me->get($username));
+            output(1, 0, self::$me->get($workid));
         }
-        $sql = "SELECT id FROM w37_user where username='{$username}'";
-        $id = self::$M->getData($sql);
+        //$sql = "SELECT id FROM w37_user where username='{$workid}'";
+        $where = array(
+            'WORK_ID' => $workid
+        );
+        $id = $user->select($where);
         if(!empty($id))
         {
-            self::$me->set($username, $username, MEMCACHE_COMPRESSED, 3600*24);
+            self::$me->set($workid, $workid, MEMCACHE_COMPRESSED, 3600*24);
         }
         return $id;
     }
